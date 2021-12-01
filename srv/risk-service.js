2852b3c7c2a6d58b592470203791981922b28ebb
byunseonuk <byunseonuk@naver.com>
2021-12-01 05:49:04 +0000

    risk-service.js Add condition Array.isArray()

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -3,13 +3,21 @@
  */
 module.exports = async (srv) => {
     srv.after('READ', 'Risks', (risks) => {
- 
-        risks.forEach((risk) => {
-            if (risk.impact >= 100000) {
-                risk.criticality = 1;
+        
+        if(Array.isArray(risks)) {
+            risks.forEach((risk) => {
+                if (risk.impact >= 100000) {
+                    risk.criticality = 1;
+                } else {
+                    risk.criticality = 2;
+                }
+            });
+        }else {
+            if (risks.impact >= 100000) {
+                risks.criticality = 1;
             } else {
-                risk.criticality = 2;
+                risks.criticality = 2;
             }
-        });
+        }
     });
 }
